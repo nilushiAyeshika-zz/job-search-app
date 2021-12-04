137ca251b211192f0271e92ecb5c23ddba7018f0
nilushi_voiceiq <nilushi@voiceiq.ai>
2021-12-04 23:06:36 +0530

    Cod refatoring

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,3 @@
-// import { createStore, applyMiddleware } from 'redux'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-// import thunk from 'redux-thunk'
-
-// import RootReducer from './rootReducer'
-
-// export const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)))
-
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
